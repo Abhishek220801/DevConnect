1be3fdc674094b9866fcde8484f03508eaff02e5
Abhishek <abhi.sankhwar22@gmail.com>
2025-12-16 21:25:51 +0530

    server client connect

--- devTinder-frontend/src/components/Login.jsx
+++ devTinder-frontend/src/components/Login.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useState } from "react"
+import axios from 'axios'
+import { useDispatch } from "react-redux"
+import { addUser } from "../utils/userSlice"
+import { useNavigate } from "react-router"
 
 const Login = () => {
+  const [emailId, setEmailId] = useState("abhishek@gmail.com")
+  const [password, setPassword] = useState("Abhi@123")
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post("http://localhost:7777/login", {
+        emailId, 
+        password,
+      }, {withCredentials: true})
+      dispatch(addUser(res.data));
+      navigate('/');
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
-    <div>
-      Login
+    <div className="flex justify-center my-[7%]">
+      <fieldset className="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-4 ">
+        <legend className="fieldset-legend">Login</legend>
+
+        <label className="label">Email</label>
+        <input
+          type="email"
+          className="input"
+          placeholder="Email"
+          value={emailId}
+          onChange={(e) => setEmailId(e.target.value)}
+        />
+
+        <label className="label">Password</label>
+        <input
+          type="password"
+          className="input"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button className="btn btn-neutral mt-4" onClick={handleLogin}>Login</button>
+      </fieldset>
     </div>
   )
 }
