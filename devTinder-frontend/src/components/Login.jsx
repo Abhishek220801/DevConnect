85c4f3c4dfe255363a1b2e25356fce4cc8d7e888
Abhishek220801 <abhi.sankhwar22@gmail.com>
2026-01-15 00:14:42 +0530

    remove default login creds

--- devTinder-frontend/src/components/Login.jsx
+++ devTinder-frontend/src/components/Login.jsx
@@ -7,8 +7,8 @@ import { BASE_URL } from "../utils/constants"
 import { toast } from "react-toastify"
 
 const Login = () => {
-  const [emailId, setEmailId] = useState("abhishek@gmail.com")
-  const [password, setPassword] = useState("Abhi@123")
+  const [emailId, setEmailId] = useState("")
+  const [password, setPassword] = useState("")
   const [firstName, setFirstName] = useState("")
   const [lastName, setLastName] = useState("")
   const [isLoginForm, setIsLoginForm] = useState(true)
