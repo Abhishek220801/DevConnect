b1cd09c91f0eb7d8149223430a545175b37465a1
Abhishek220801 <abhi.sankhwar22@gmail.com>
2026-01-17 12:39:36 +0530

    BASE+STATIC url update

--- devTinder-frontend/src/utils/constants.js
+++ devTinder-frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = "/api";
-// export const STATIC_URL = "/";  
-export const BASE_URL = "http://localhost:7777/api";
-export const STATIC_URL = "http://localhost:7777";  
+export const BASE_URL = "/api";
+export const STATIC_URL = "/";  
+// export const BASE_URL = "http://localhost:7777/api";
+// export const STATIC_URL = "http://localhost:7777";  
